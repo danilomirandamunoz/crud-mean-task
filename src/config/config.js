91ca15251a73c3f81efe88aa62d7fef111ea3420
danilo <danilo.miranda.munoz@gmail.com>
2018-08-07 17:26:23 -0400

    cambios

--- src/config/config.js
+++ src/config/config.js
@@ -7,10 +7,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //variable para poner la cadena de conexion a la BD
 let urlDB;
 
-if (process.env.NODE_ENV == 'dev') {
+if (process.env.NODE_ENV == 'devd') {
     urlDB = 'mongodb://localhost:27017/mean-db-task';
 } else {
-    urlDB = process.env.MONGO_URI;
+    urlDB = "mongodb://dmirandam-a123456:a123456@ds115472.mlab.com:15472/crud-mean-dmirandam";
 }
 
 process.env.urlDB = urlDB;
